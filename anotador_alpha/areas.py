c1a2dea22486aa2ade23d9ca018f0abc1bb12810
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2020-11-03 22:24:19 -0300

    implementa categoria e tags para cada objeto Area
    - desenha/mostra nome da categoria na área (WIP)
    - edição ativa em CRIAR e SELEC...
    - método cat_and_tag_selection() é chamado pela interface no mouseReleased()

--- anotador_alpha/areas.py
+++ anotador_alpha/areas.py
@@ -1,4 +1,9 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
+from __future__ import unicode_literals
+
+from copy import deepcopy
+from categorias import draw_cats, seleciona_cat, seleciona_tag, active_cat
+import interface
 
 class Area:
 
@@ -10,23 +15,40 @@ class Area:
         self.selected = False
         self.over = False
         self.area = self.w * self.h
-        self.cobertura = 1 # 100%
+        self.cobertura = 1  # 100%
+        self.tags = deepcopy(Area.tags)
+        self.categorias = deepcopy(Area.categorias)
+        self.cat = ""
 
     def display(self, mp):
+        textSize(11)
         stroke(0)
+        fill(0)
         if self.selected:
             stroke(200, 0, 0)
+            fill(100, 0, 0)
             strokeWeight(3)
         elif self.over and not mp:
             strokeWeight(5)
             self.over = False
         else:
             strokeWeight(2)
+        self.cat = active_cat(self.categorias)
+        text('{} ({:2.0%})'.format(self.cat, self.cobertura),
+             self.x + 10, self.y + 20)
         fill(0, 20)
         rect(self.x, self.y, self.w, self.h)
-        fill(255)
-        text('{:2.0%}'.format(self.cobertura), self.x, self.y + 20)
+
+        ma = interface.interface.modo_ativo
+        if (self.cobertura != 1 and self.selected and
+                ma in (interface.SELEC, interface.CRIAR)):
+            draw_cats(self.categorias)
+            draw_cats(self.tags)
+
+    def cat_and_tag_selection(self):
+        seleciona_cat(self.categorias)
+        seleciona_tag(self.tags)
 
     def mouse_over(self):
         return (self.x < mouseX < self.x + self.w
-            and self.y < mouseY < self.y + self.h)
+                and self.y < mouseY < self.y + self.h)
