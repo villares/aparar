b5e4f4c4c6a1ab74e1646d356f418a43fd9bc61f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2020-11-06 13:58:39 -0300

    interface cleanup

--- anotador_beta/pranchas.py
+++ anotador_beta/pranchas.py
@@ -5,7 +5,7 @@ import interface
 
 class Prancha:
 
-    atual = 0
+    i_atual = 0
     pranchas = []
     path_sessao = ""
     nome_sessao = ""
@@ -16,16 +16,16 @@ class Prancha:
         self.nome = nome       # AAA_BBB_CCCxxxxxx
         self.init_ids()
         self.rot = 0
-        
+
     def init_ids(self):
-        nome = self.nome    
+        nome = self.nome
         sep_pos = nome.find("_")
         if sep_pos > 0:
-               self.ida = nome[:sep_pos]    # AAA ou AAAA
-               self.idb = nome[sep_pos:sep_pos + 3]   # BBB
-               self.idc = nome[sep_pos + 3:sep_pos + 6]  # CCC
+            self.ida = nome[:sep_pos]    # AAA ou AAAA
+            self.idb = nome[sep_pos:sep_pos + 3]   # BBB
+            self.idc = nome[sep_pos + 3:sep_pos + 6]  # CCC
         else:
-                self.ida = self.idb = self.idc = nome
+            self.ida = self.idb = self.idc = nome
 
     def display_areas(self, mp):
         ma = interface.modo_ativo
@@ -70,49 +70,61 @@ class Prancha:
         total = len(cls.pranchas) - 1  # home não conta!
         if nome != '000':  # prancha "home" teste
             fill(200, 0, 0)
-            texto = "({:03}/{:03}) {}".format(cls.atual, total, nome)
+            texto = "({:03}/{:03}) {}".format(cls.i_atual, total, nome)
         else:
             fill(0)
             texto = "pranchas carregadas: {}".format(total)
         text(texto, 780, 30)
 
-
     @classmethod
     def nome_prancha_atual(cls):
-        return cls.pranchas[cls.atual].nome
+        return cls.pranchas[cls.i_atual].nome
+
+    @classmethod
+    def img_prancha_atual(cls, imagens):
+        """devolve imagem na prancha atual ou None"""
+        return imagens.get(cls.nome_prancha_atual().lower())
 
     @classmethod
     def display_imagem_atual(cls, imagens):
-        rot =  cls.pranchas[cls.atual].rot
-        img = imagens.get(cls.nome_prancha_atual().lower())
-        if img is not None:
-            fator = cls.calc_fator(img, rot == 1 or rot == 3)
+        img, rot, fator = cls.imagem_rot_fator_atual(imagens)
+        if img:
             image_rot(img, rot, interface.OX, interface.OY,
-                img.width * fator, img.height * fator)
+                      img.width * fator, img.height * fator)
         else:
             cls.avisos("IMAGEM NÃO CARREGADA")
-            
+
+    @classmethod
+    def imagem_rot_fator_atual(cls, imagens):
+        img = cls.img_prancha_atual(imagens)
+        if img:
+            rot = cls.pranchas[cls.i_atual].rot
+            fator = cls.calc_fator(img, rot == 1 or rot == 3)
+            return img, rot, fator
+        else:
+            return None, None, None
+
     @classmethod
     def calc_fator(cls, img, rotated=False):
         if not rotated:
             return float(height - (interface.OY + interface.rodape)) / img.height
         else:
             return float(height - (interface.OY + interface.rodape)) / img.width
-            
+
     @classmethod
     def display_areas_atual(cls, mp):
-        cls.pranchas[cls.atual].display_areas(mp)
+        cls.pranchas[cls.i_atual].display_areas(mp)
 
     @classmethod
     def get_areas_atual(cls):
-        return cls.pranchas[cls.atual].areas
+        return cls.pranchas[cls.i_atual].areas
 
     @classmethod
     def desselect_all(cls):
         for r in cls.get_areas_atual():
             r.selected = False
-            
-    @classmethod        
+
+    @classmethod
     def avisos(cls, t=None):
         if cls.carregando:
             t = "CARREGANDO IMAGENS"
@@ -120,10 +132,10 @@ class Prancha:
             push()
             fill(200, 0, 0)
             textSize(24)
-            text(t, width / 2, height / 2)
+            text(t, interface.OX + 10, interface.OY + 30)
             pop()
-            
-            
+
+
 def image_rot(img, rot, x, y, w=None, h=None):
     w = w or img.width
     h = h or img.height
@@ -135,11 +147,11 @@ def image_rot(img, rot, x, y, w=None, h=None):
     elif rot == 2:
         translate(x + w, y + h)
         rotate(PI)
-        image(img, 0, 0, w, h)    
+        image(img, 0, 0, w, h)
     elif rot == 3:
         translate(x, y + w)
         rotate(HALF_PI + PI)
         image(img, 0, 0, w, h)
     else:
-        image(img, x, y, w, h)    
-    popMatrix()     
+        image(img, x, y, w, h)
+    popMatrix()
