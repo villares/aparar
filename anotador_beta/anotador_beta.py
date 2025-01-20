ea4795e098c12f34e4b5d78e79a31348267f6ff8
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-01-20 16:36:18 -0300

    Update anotador_beta.py

--- anotador_beta/anotador_beta.py
+++ anotador_beta/anotador_beta.py
@@ -7,8 +7,31 @@ Colaboradores: Carolina Celete, Raissa Rodrigues, Larissa Negris de Souza e Alex
 
 Embrião do código de retângulos reconfiguráveis aproveitado do projeto
 co-criar co-mover de Graziele Lautenschlaeger https://github.com/grazilaut/co_criar_co_mover
+
+---
+
+APARAR - Anotador de Pranchas para Análise e Registro de Áreas Relativas
+Copyright (C) 2025 Alexandre B A Villares
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """
 
+# [WIP] ESTA VERSÂO COM PY5 AINDA NÂO ESTÁ FUNCIONANDO 
+#     [ ] Migrar para Pathlib
+# [WIP] Checar branches (como "clean_up") se melhorias foram incorporadas
+#     [ ] ? "primazia das coordenadas de prancha"
+#     [ ] ? "nota livre"
 # [WIP] Modo diagrama
 #     [ ] Patterns PB para legenda de categorias
 # [ ] Opção de aumentar o rodapé para corrigir problema de pranchas longas!
